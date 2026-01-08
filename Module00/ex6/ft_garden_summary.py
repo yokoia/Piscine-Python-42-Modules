5c056840189e8ada1e90d0eb8c32f27025285168
Youssra Koia <ykoia@c4r7p8.1337.ma>
2026-01-08 13:39:33 +0100

    M00

---
+++ Module00/ex6/ft_garden_summary.py
@@ -0,0 +1,5 @@
+def ft_garden_summary():
+    name = input("Enter garden name: ")
+    number = input("Enter number of plants: ")
+    print("Garden:", name, "\nPlants:", number)
+    print("Status: Growing well!")
