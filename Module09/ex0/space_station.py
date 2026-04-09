b081216c79b6b8d07fa11b6bb4c4d332102e357b
Youssra Koia <ykoia@c3r5p3.1337.ma>
2026-04-09 16:33:12 +0100

    m9

---
+++ Module09/ex0/space_station.py
@@ -0,0 +1,60 @@
+
+
+from pydantic import BaseModel, Field, ValidationError
+from datetime import datetime
+from typing import Optional
+
+
+class SpaceStation(BaseModel):
+    station_id: str = Field(min_length=3, max_length=10)
+    name: str = Field(min_length=1, max_length=50)
+    crew_size: int = Field(ge=1, le=20)
+    power_level: float = Field(ge=0.0, le=100.0)
+    oxygen_level: float = Field(ge=0.0, le=100.0)
+    last_maintenance: datetime
+    is_operational: bool = Field(default=True)
+    notes: Optional[str] = Field(max_length=200)
+
+
+def main() -> None:
+    try:
+        print("Space Station Data Validation")
+        print("========================================")
+        station = SpaceStation(station_id="ISS001",
+                               name="International Space Station",
+                               crew_size="6",
+                               power_level=85.5,
+                               oxygen_level=92.3,
+                               last_maintenance="2024-04-09T12:00:00",
+                               notes="International Space Station has been "
+                               "created"
+                               )
+        print("Valid station created:")
+        print("ID:", station.station_id)
+        print("Name", station.name)
+        print(f"Crew: {station.crew_size} people")
+        print(f"Power: {station.power_level}%")
+        print(f"oxygen_level: {station.oxygen_level}%")
+        if station.is_operational is True:
+            print("Status: Operational")
+        else:
+            print("Status: Not operational")
+
+        print("\n========================================")
+        station = SpaceStation(station_id="ISS001",
+                               name="International Space Station",
+                               crew_size=22,
+                               power_level=85.5,
+                               oxygen_level=92.3,
+                               last_maintenance="2024-04-09T12:00:00",
+                               notes="International Space Station has been "
+                               "created"
+                               )
+    except ValidationError:
+        print("Expected validation error:")
+        print("Input should be less than or equal to 20")
+
+
+
+
+main()
